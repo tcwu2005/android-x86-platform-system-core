4225f6c62b3b4b087bbb7be56ea6e6d521a80a0d
Chih-Wei Huang <cwhuang@linux.org.tw>
2010-07-21 15:34:39 +0800

    add back libdiskconfig_host
    This is required by editdisklbl on building usb_img target.

--- libdiskconfig/Android.mk
+++ libdiskconfig/Android.mk
@@ -5,15 +5,25 @@ ifneq ($(TARGET_SIMULATOR),true)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
+commonSources := \
 	diskconfig.c \
 	diskutils.c \
 	write_lst.c \
 	config_mbr.c
 
+LOCAL_SRC_FILES := $(commonSources)
 LOCAL_MODULE := libdiskconfig
 LOCAL_SYSTEM_SHARED_LIBRARIES := libcutils liblog libc
 
 include $(BUILD_SHARED_LIBRARY)
+
+# static library for host
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(commonSources)
+LOCAL_CFLAGS := -O2 -g -W -Wall -Werror -D_LARGEFILE64_SOURCE
+LOCAL_MODULE := libdiskconfig_host
+LOCAL_STATIC_LIBRARIES := libcutils
+
+include $(BUILD_HOST_STATIC_LIBRARY)
 
 endif  # ! TARGET_SIMULATOR
