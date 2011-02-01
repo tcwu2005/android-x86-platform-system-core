ecce05bb6671e05c56a04bc774ee04cbe76fb647
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-02-01 15:47:03 +0800

    init: change firmware_dirs for Android-x86

--- init/devices.cpp
+++ init/devices.cpp
@@ -49,9 +49,13 @@
 #include "log.h"
 
 #define SYSFS_PREFIX    "/sys"
+#if defined(__i386__) || defined(__x86_64__)
+static const char *firmware_dirs[] = { "/system/lib/firmware" };
+#else
 static const char *firmware_dirs[] = { "/etc/firmware",
                                        "/vendor/firmware",
                                        "/firmware/image" };
+#endif
 
 extern struct selabel_handle *sehandle;
 
