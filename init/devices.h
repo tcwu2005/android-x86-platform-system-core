9cbe04b902c07e54f426531b43cf8cce097684be
Jianxun Zhang <jianxun.zhang@intel.com>
2012-07-02 11:32:30 -0700

    add modprobe-like capability and automatic ueventd loading
    Author: Jianxun Zhang <jianxun.zhang@intel.com>
    Author: Daniel Leung <daniel.leung@intel.com>
    Author: Andrew Boie <andrew.p.boie@intel.com>
    
    - insmod_by_dep() added to libcutils; loads a module into kernel.
    Modules the target module depends on will be loaded first. Loading
    will be stopped when an error occurs.
    
    - rmmod_by_dep() added to libcutils; removes a module from kernel.
    It also tries to remove other modules the target module depends
    on until an error occurs.
    
    - Implement wildcard matching for ueventd rules.
    The PCI and USB addresses for devices can change from devices
    from devices for a particular class of peripheral, for example,
    bluetooth. The ueventd rules created with these addresses are
    then device-specific.
    
    This changes the way ueventd rules with wildcard are handled.
    Instead of matching just the prefix with a trailing wildcard,
    now these rules can have wildcard anywhere in the rule.
    The wildcard matching is implemented using fnmatch(), where
    its matching is simliar to shell pathname expansion. It suits
    this particular usage model well.
    
    For example, instead of creating a rule to match:
    
      /sys/devices/pci0000:00/0000:00:1d.0/usb2/2-1/2-1.3/2-1.3:1.0/bluetooth/hci0/rfkill*
    
    , this would suffice:
    
      /sys/devices/*/bluetooth/hci0/rfkill*
    
    - Let ueventd auto-load kernel modules. Implements the functionality
    for ueventd to auto-load kernel modules when uevents are triggered.
    Since /system may not be mounted when uevents are fired,
    a deferred loading mechanism is implemented. Once mapping of
    module and alias is available, these modules are then loaded.
    Modules can also be blacklisted so they will not be loaded
    automatically. One example would be the Wifi driver, as
    Android's has to control its loading and unloading.
    
    - add 'probemod' builtin command. This command accepts the name of a
    kernel module plus a set of command line arguments. The module will
    be loaded, along with all its dependencies, using the libcutils
    insmod_by_dep() API.
    
    - Drivers in kernel can request modules by launching a program in
    user space, the program's path by default is "/sbin/modprobe".
    Because Android system has no modprobe and ueventd is the only
    program handling the module aliases so far, This patch provides a
    cheap approach to handle kernel's requests in ueventd executable.
    
    - Add new builtin init command "coldboot". The main purpose is to
    provide an approach in init.*.rc files to fire uevents for devices
    under the path which is passed as the the argument. This should be
    called after /system is mounted so any queued events that need to
    load a module can be fired.

--- init/devices.h
+++ init/devices.h
@@ -21,8 +21,10 @@
 
 extern void handle_device_fd();
 extern void device_init(void);
+extern int module_probe(const char *alias);
 extern int add_dev_perms(const char *name, const char *attr,
                          mode_t perm, unsigned int uid,
-                         unsigned int gid, unsigned short prefix);
+                         unsigned int gid, unsigned short wildcard);
 int get_device_fd();
+void coldboot(const char *path);
 #endif	/* _INIT_DEVICES_H */
--- init/keywords.h
+++ init/keywords.h
@@ -1,5 +1,6 @@
 
 #ifndef KEYWORD
+int do_builtin_coldboot(int nargs, char **args);
 int do_chroot(int nargs, char **args);
 int do_chdir(int nargs, char **args);
 int do_class_start(int nargs, char **args);
@@ -15,6 +16,7 @@ int do_mkdir(int nargs, char **args);
 int do_mount_all(int nargs, char **args);
 int do_mount(int nargs, char **args);
 int do_powerctl(int nargs, char **args);
+int do_probemod(int nargs, char **args);
 int do_restart(int nargs, char **args);
 int do_restorecon(int nargs, char **args);
 int do_rm(int nargs, char **args);
@@ -69,6 +71,7 @@ enum {
     KEYWORD(oneshot,     OPTION,  0, 0)
     KEYWORD(onrestart,   OPTION,  0, 0)
     KEYWORD(powerctl,    COMMAND, 1, do_powerctl)
+    KEYWORD(probemod,    COMMAND, 1, do_probemod)
     KEYWORD(restart,     COMMAND, 1, do_restart)
     KEYWORD(restorecon,  COMMAND, 1, do_restorecon)
     KEYWORD(rm,          COMMAND, 1, do_rm)
@@ -98,6 +101,7 @@ enum {
     KEYWORD(loglevel,    COMMAND, 1, do_loglevel)
     KEYWORD(load_persist_props,    COMMAND, 0, do_load_persist_props)
     KEYWORD(ioprio,      OPTION,  0, 0)
+    KEYWORD(coldboot,    COMMAND, 1, do_builtin_coldboot)
 #ifdef __MAKE_KEYWORD_ENUM__
     KEYWORD_COUNT,
 };
