7dec791304e7d9e2c5e1222d94dd57ace00cf59f
Daniel Leung <daniel.leung@intel.com>
2012-10-22 16:32:52 -0700

    libsuspend: Use autosleep first if available
    Change the order to look for sysfs nodes to do autosleep first,
    then early suspend.
    
    Change-Id: Iff34b9354dd8e5104befd05e8097962d7f7eee7f
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- libsuspend/autosuspend.c
+++ libsuspend/autosuspend.c
@@ -33,12 +33,12 @@ static int autosuspend_init(void)
         return 0;
     }
 
-    autosuspend_ops = autosuspend_earlysuspend_init();
+    autosuspend_ops = autosuspend_autosleep_init();
     if (autosuspend_ops) {
         goto out;
     }
 
-    autosuspend_ops = autosuspend_autosleep_init();
+    autosuspend_ops = autosuspend_earlysuspend_init();
     if (autosuspend_ops) {
         goto out;
     }
