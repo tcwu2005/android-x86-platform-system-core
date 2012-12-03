8e073620710b8854fa9ece417c5103381eb91df5
Bruce E. Robertson <bruce.e.robertson@intel.com>
2012-12-03 13:01:28 -0800

    ADBD: set sent file permissions per android fs config table.
    This fix allows the standard CTS installation to succeed while
    preserving the previous fix's intent to allow init to operate
    properly.
    
    The first CTS APK installation failed when adbd, the adb daemon, on
    the target invoked pm (package manager). "pm" runs as another user
    than adbd and could not open the APK because adbd had cleared the
    "other" read permission.
    
    For: AXIA-813
    Change-Id: If27a628072f51ffac78db7b5448670ea4666575f
    Signed-off-by: Bruce E. Robertson <bruce.e.robertson@intel.com>
    
    Conflicts:
    	adb/file_sync_service.c

--- adb/Android.mk
+++ adb/Android.mk
@@ -117,6 +117,8 @@ LOCAL_SRC_FILES := \
 	usb_linux_client.c \
 	log_service.c
 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include/private
+
 LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wall -Wno-unused-parameter
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 
